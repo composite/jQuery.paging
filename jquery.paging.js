760349c43242d8f8a8b4b4217abd7422d3e015d2
Composite <ukjinplant@msn.com>
2012-10-11 09:35:47 +0900

    0.1.5 update
    first and last button added. and content can appends substitution.
    new option 'append' : If you set this true, paging will generate content
    without delete target's content. default is false.
    new Substitution {6} : the last page (equal of 'max' option.).

--- jquery.paging.js
+++ jquery.paging.js
@@ -1,26 +1,27 @@
 /**************************************************************************************
- * jQuery Paging 0.1.0
+ * jQuery Paging 0.1.5
  * by composite (ukjinplant@msn.com)
  * http://composite.tistory.com
  * This project licensed under a MIT License.
  **************************************************************************************/;
 (function($){
-	var defs={
-		item:'a',next:'[{5}&gt;]',prev:'[{4}&lt;]',format:'[{0}]',
+	//default properties.
+	var a=/a/i,defs={
+		item:'a',next:'[&gt;{5}]',prev:'[{4}&lt;]',format:'[{0}]',
 		itemClass:'paging-item',sideClass:'paging-side',
-		itemCurrent:'selected',length:10,max:1,current:1
-		,href:'#{0}',event:true
+		itemCurrent:'selected',length:10,max:1,current:1,append:false
+		,href:'#{0}',event:true,first:'[1&lt;&lt;]',last:'[&gt;&gt;{6}]'
 	},format=function(str){
 		var arg=arguments;
 		return str.replace(/\{(\d+)\}/g,function(m,d){
-			if(+d<0) return "";
+			if(+d<0) return m;
 			else return arg[+d+1]||"";
 		});
 	},item,make=function(op,page,cls,str){
 		item=document.createElement(op.item);
 		item.className=cls;
-		item.innerHTML=format(str,page,op.length,op.start,op.end,op.start-op.length,op.end+op.length);
-		if(item.nodeName.toLowerCase()=='a') item.href=format(op.href,page);
+		item.innerHTML=format(str,page,op.length,op.start,op.end,op.start-1,op.end+1,op.max);
+		if(a.test(op.item)) item.href=format(op.href,page);
 		if(op.event){
 			$(item).bind('click',function(e){
 				var fired=true;
@@ -28,10 +29,13 @@
 				if(fired==undefined||fired)
 					op.origin.paging($.extend({},op,{current:page}));
 			}).appendTo(op.origin);
+			//bind event for each elements.
 			var ev='on';
 			switch(str){
 				case op.prev:ev+='prev';break;
 				case op.next:ev+='next';break;
+				case op.first:ev+='first';break;
+				case op.last:ev+='last';break;
 				default:ev+='item';break;
 			}
 			if($.isFunction(op[ev])) op[ev].call(item,page,op);
@@ -46,14 +50,21 @@
 		op.start=Math.floor((op.current-1)/op.length)*op.length+1;
 		op.end=op.start-1+op.length;
 		if(op.end>op.max) op.end=op.max;
-		this.html('');
+		if(!op.append) this.empty();
 		//prev button
-		if(op.current>op.length) make(op,op.start-1,op.sideClass,op.prev);
+		if(op.current>op.length){
+			make(op,1,op.sideClass,op.first);
+			make(op,op.start-1,op.sideClass,op.prev);
+		}
 		//pages button
 		for(var i=op.start;i<=op.end;i++)
 			make(op,i,op.itemClass+(i==op.current?' '+op.itemCurrent:''),op.format);
 		//next button
-		if(op.current<=Math.floor(op.max/op.length)*op.length)
+		if(op.current<=Math.floor(op.max/op.length)*op.length){
 			make(op,op.end+1,op.sideClass,op.next);
+			make(op,op.max,op.sideClass,op.last);
+		}
+			
+		//last button
 	};
 })(jQuery);
--- readme.txt
+++ readme.txt
@@ -3,7 +3,6 @@ jQuery.Paging
 a Simple Paging for jQuery
 
 usage : $('#paging').paging({max:50});
-will display as : [1][2][3][4][5][6][7][8][9][10][>]
 
 options :
 	item : item tag element name, default is "a".
@@ -11,12 +10,15 @@ options :
 	itemCurrent : current page element class. this class will append with item class, default is "selected".
 	format : a content of page element, default is "[%d]".
 	sideClass : class for next and prev button, default is "paging-side".
-	next : a content of next page element. default is "[&gt;]" ("[>]")
-	prev : a content of prev page element. default is "[&lt;]" ("[<]")
+	next : a content of next page element. default is "[&gt;{5}]" ("[>{5}]")
+	prev : a content of prev page element. default is "[{4}&lt;]" ("[{4}<]")
+	first : a content of first page element. default is "[1&lt;&lt;]"
+	last : a content of last page element. default is "[&gt;&gt;{6}]"
 	length : length of display pages. default is 10.
 	max : defines max page. default is 1.
 	current : defines default page. default is 1.
 	href : if item tag name is 'a', activate link each item. default is "#%d"
+	append : If you set true, append paging content instead of replace target's content. default is false.
 	event : activate default events. If you click any item, change paging dynamically. usally for ajax. default is true.
 	>these event will only affected when event=true.
 	onclick : fire event if clicked any items. if you returns false, will not refresh paging.
@@ -24,12 +26,20 @@ options :
 	onnext : init additional operation for next button. 'this' scopes next button element(plain DOM. not jQuery!)
 	onitem : init additional operation for each page item button. 'this' scopes any page item element.
 
-subtitudes (for format,next,prev property)
+Substitution (for format,next,prev,href,first,last property)
 {0} = item page
 {1} = page length
 {2} = start page
 {3} = end page
-{4} = start page of prev part
-{5} = end page of next part
+{4} = last page of prev part
+{5} = start page of next part
+{6} = max page
 
+changelog
+
+0.1.0 : initial release.
+0.1.5 :
+first and last button added. and content can appends substitution.
+new option 'append' : If you set this true, paging will generate content without delete target's content. default is false.
+new Substitution {6} : the last page (equal of op.max).
 License : MIT
